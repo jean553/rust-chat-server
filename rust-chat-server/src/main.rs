40320d4f6db190779571c561095db785de5f2e22
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-07-04 13:47:39 +0200

    add documentation of the function that handles TCP requests

--- rust-chat-server/src/main.rs
+++ rust-chat-server/src/main.rs
@@ -1,4 +1,20 @@
-use std::net::{TcpListener};
+//! Basic TCP server
+
+use std::net::{
+    TcpListener,
+    TcpStream,
+};
+
+/// Handles received TCP requests
+///
+/// TODO: define the function
+///
+/// # Arguments:
+///
+/// * `stream` - TCP stream between the server and the new connected client
+fn handle_request(stream: TcpStream) {
+    println!("New client connected.");
+}
 
 fn main() {
 
