bb14f967a90b1c84b980d82df3041fb730baa2ad
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-07-04 13:25:50 +0200

    log if a connection succeeds or not

--- rust-chat-server/src/main.rs
+++ rust-chat-server/src/main.rs
@@ -2,5 +2,17 @@ use std::net::{TcpListener};
 
 fn main() {
 
-    let listener = TcpListener::bind("127.0.0.1:9090").unwrap();
+    let listener = TcpListener::bind("0.0.0.0:9090").unwrap();
+
+    for income in listener.incoming() {
+
+        match income {
+            Ok(_) => {
+                println!("New client connected.");
+            }
+            Err(_) => {
+                println!("Client connection failed.");
+            }
+        }
+    }
 }
