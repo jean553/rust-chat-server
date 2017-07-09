047fcc8e858da41bfec0019412f1c06b819243d1
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-07-09 14:46:30 +0200

    create channel to send messages from one thread to another

--- rust-chat-server/src/main.rs
+++ rust-chat-server/src/main.rs
@@ -25,6 +25,7 @@ use std::sync::mpsc;
 fn handle_request(
     mut stream: TcpStream,
     client_id: u8,
+    sender: mpsc::Sender<String>,
 ) {
     stream.write("Welcome to rust-chat-server\n".as_bytes()).unwrap();
 
@@ -47,24 +48,58 @@ fn handle_request(
                         break;
                     },
                     Some(&_) => {
-                        println!(
+
+                        let message_to_send = format!(
                             "Client {} sent message: {}",
                             client_id,
                             message,
                         );
 
-                        message.clear();
+                        match sender.send(message_to_send.to_string()) {
+                            Ok(_) => {
+                                message.clear();
+                            },
+                            Err(_) => {
+                                println!(
+                                    "Error: cannot read message from client {}",
+                                    client_id,
+                                );
+
+                                break;
+                            }
+                        };
                     }
                 };
             }
             _ => {
 
                 println!(
-                    "Error: cannot read message from client {}",
+                    "Error: cannot read request from client {}",
                     client_id,
                 );
 
                 break;
+            }
+        }
+    }
+}
+
+/// Started by an independant thread that listens
+/// for new messages and sends them to every thread
+///
+/// # Arguments:
+///
+/// * `receiver` - Channel receiver to get messages
+fn receive_messages(receiver: &mpsc::Receiver<String>) {
+
+    loop {
+        let message = receiver.recv(); // blocking IO
+
+        match message {
+            Ok(value) => {
+                println!("Received message: {}", value);
+            }
+            Err(_) => {
             }
         }
     }
@@ -81,6 +116,10 @@ fn main() {
         mpsc::Receiver<String>
     ) = mpsc::channel();
 
+    spawn(move || {
+        receive_messages(&receiver);
+    });
+
     for income in listener.incoming() {
 
         match income {
@@ -92,10 +131,13 @@ fn main() {
                     client_address,
                 );
 
+                let sender = sender.clone();
+
                 spawn(move || {
                     handle_request(
                         stream,
                         clients_count,
+                        sender,
                     );
                 });
 
